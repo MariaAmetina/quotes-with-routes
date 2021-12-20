1be7027b3242629eb84c41f221dfa789105c43d9
MariiaAmetina <ametina.ms@gmail.com>
2021-12-20 21:38:28 +0300

    quotes details and 'add a quote'

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,12 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Mary", text: "Learning React is fun!" },
+  { id: "q2", author: "Paul", text: "Learning Node is great!" },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,14 +1,25 @@
 import { useParams, Route } from "react-router-dom";
 
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Mary", text: "Learning React is fun!" },
+  { id: "q2", author: "Paul", text: "Learning Node is great!" },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
 
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <h1>No quote found!</h1>;
+  }
+
   return (
     <>
-      <h1>Quote Detail</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path="/quotes/:quoteId/comments">
         <Comments />
       </Route>
