1ae4241553cf791968cb15d6614183263e88ae49
MariiaAmetina <ametina.ms@gmail.com>
2021-12-22 13:25:58 +0300

    lazy loading implemented

--- src/App.js
+++ src/App.js
@@ -1,33 +1,45 @@
+import React, { Suspense } from "react"; //Suspense - для показа инфы, что lazy loading контент грузится
 import { Route, Switch, Redirect } from "react-router-dom";
 
-import AllQuotes from "./pages/AllQuotes";
-import QuoteDetail from "./pages/QuoteDetail";
-import NewQuote from "./pages/NewQuote";
-import NotFound from "./pages/NotFound";
 import Layout from "./components/layout/Layout";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+//implementing lazy loading
+const NewQuote = React.lazy(() => import("./pages/NewQuote"));
+const QuoteDetail = React.lazy(() => import("./pages/QuoteDetail"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
+const AllQuotes = React.lazy(() => import("./pages/AllQuotes"));
 
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/quotes" />
-        </Route>
-        <Route path="/quotes" exact>
-          <AllQuotes />
-        </Route>
-        <Route path="/quotes/:quoteId">
-          <QuoteDetail />
-        </Route>
-        <Route path="/new-quote">
-          <NewQuote />
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>{" "}
-        {/* если ни одна из предыдущих ссылок не подойдет (юзер введет неверный адрес), то сработает эта строка.
-         * - это любая ссылка, поэтому она должна идти последней */}
-      </Switch>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/quotes" />
+          </Route>
+          <Route path="/quotes" exact>
+            <AllQuotes />
+          </Route>
+          <Route path="/quotes/:quoteId">
+            <QuoteDetail />
+          </Route>
+          <Route path="/new-quote">
+            <NewQuote />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+          {/* если ни одна из предыдущих ссылок не подойдет (юзер введет неверный адрес), то сработает эта строка.
+           * - это любая ссылка, поэтому она должна идти последней */}
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
